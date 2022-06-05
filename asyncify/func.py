ff4660a2d9111242b09544f1c80ab0228a8e898e
chawkk6404 <chawk_k@outlook.com>
2022-06-05 12:31:45 -0700

    Remove future choice

--- asyncify/func.py
+++ asyncify/func.py
@@ -3,62 +3,44 @@ import functools
 
 from typing import (
     Any,
-    Awaitable,
     Callable,
     Coroutine,
-    Literal,
-    overload,
-    TypeAlias,
     TypeVar,
     ParamSpec,
-    Union
 )
-
 
 P = ParamSpec('P')
 T = TypeVar('T')
 
 
-def _make_future_func(func: Callable[P, T]) -> Callable[P, Awaitable[T]]:
-    @functools.wraps(func)
-    def future_func(*args: P.args, **kwargs: P.kwargs) -> Awaitable[T]:
-        partial = functools.partial(func, *args, **kwargs)
+def asyncify_func(func: Callable[P, T]) -> Callable[P, Coroutine[Any, Any, T]]:
+    """
+    Make a synchronous function into an asynchronous function by running it in an executor.
 
-        loop = asyncio.get_event_loop()
-        return loop.run_in_executor(None, partial)
+    Usage
+    -----------
+    .. code:: py
 
-    return future_func
+        import asyncio
 
+        import asyncify
+        import requests
 
-def _make_coro_func(func: Callable[P, T]) -> Callable[P, Coroutine[Any, Any, T]]:
+        @asyncify_func
+        def get(url: str) -> str:
+            return requests.get(url).text
+
+        # `get` is no longer a blocking function
+        # it is now a coroutine function
+
+        async def main():
+            text = await get('https://python.org')
+
+        asyncio.run(main())
+    """
     @functools.wraps(func)
     async def async_func(*args: P.args, **kwargs: P.kwargs) -> T:
-        partial = functools.partial(func, *args, **kwargs)
-
-        loop = asyncio.get_event_loop()
-        return await loop.run_in_executor(None, partial)
+        return await asyncio.to_thread(func, *args, **kwargs)
 
     return async_func
 
-
-@overload
-def asyncify_func(return_future: Literal[True]) -> Callable[[Callable[P, T]], Callable[P, Awaitable[T]]]:
-    ...
-
-
-@overload
-def asyncify_func(return_future: Literal[False]) -> Callable[[Callable[P, T]], Callable[P, Coroutine[Any, Any, T]]]:
-    ...
-
-
-def asyncify_func(return_future: bool = False) -> Callable[[Callable[P, T]], Callable[P, Union[
-    Awaitable[T],
-    Coroutine[Any, Any, T]
-]]]:
-    def inner(func: Callable[P, T]) -> Callable[P, Union[Awaitable[T], Coroutine[Any, Any, T]]]:
-        if return_future:
-            new_func = _make_future_func(func)
-        else:
-            new_func = _make_coro_func(func)
-        return new_func
-    return inner
