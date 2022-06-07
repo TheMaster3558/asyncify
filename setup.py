ebc1fe9ed98e02214534ed662375d5b9684abf07
chawkk6404 <92063273+chawkk6404@users.noreply.github.com>
2022-06-07 14:03:20 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 
-with open('../discord/__init__.py') as f:
+with open('./asyncify/__init__.py') as f:
     version = re.search(r'^__version__\s*=\s*[\'"]([^\'"]*)[\'"]', f.read(), re.MULTILINE).group(1)
     
     
