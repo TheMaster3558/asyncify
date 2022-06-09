49ae7c9de14c86573fbdc215f6cab37b0930614f
chawkk6404 <92063273+chawkk6404@users.noreply.github.com>
2022-06-09 10:45:18 -0700

    Update imports

--- asyncify/__init__.py
+++ asyncify/__init__.py
@@ -1,6 +1,6 @@
-from .func import asyncify_func, set_max_threads, syncify_func
+from .func import asyncify_func, syncify_func
 from .iterable import async_iter
 
 
-__version__ = '1.0.0a'
 __author__ = 'The Master'
+__version__ = '1.0.0a'
