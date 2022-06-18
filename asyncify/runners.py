e814d6486543b87fff8a00c4588aaa7d4f725dc5
chawkk6404 <chawk_k@outlook.com>
2022-06-17 21:57:50 -0700

    Update run

--- asyncify/runners.py
+++ asyncify/runners.py
@@ -1,4 +1,5 @@
 import asyncio
+import sys
 from typing import Any, Coroutine, TypeVar
 
 
@@ -10,65 +11,64 @@ __all__ = (
 T = TypeVar('T')
 
 
-def run(coro: Coroutine[Any, Any, T], *, debug: bool = False) -> T:
-    """
-    An implementation of `asyncio.run <https://docs.python.org/3/library/asyncio-task.html?highlight=asyncio%20run#asyncio.run>`_
-    for users below `Python 3.7`.
+if sys.version_info >= (3, 7) and 'sphinx' in sys.modules:
+    from asyncio import run
+else:
+    def run(coro: Coroutine[Any, Any, T], *, debug: bool = False) -> T:
+        """
+        An implementation of `asyncio.run <https://docs.python.org/3/library/asyncio-task.html?highlight=asyncio%20run#asyncio.run>`_
+        for users below `Python 3.7`.
 
-    .. note::
-        This is very similar to Python 3.7's original `asyncio.run`
+        .. note::
+            This is very similar to Python 3.7's original `asyncio.run` and will use `asyncio`'s run if
+            the `Python` version is 3.7 or above.
 
 
-    Parameters
-    -----------
-    coro: ``Coroutine``
-        The coroutine to run.
-    debug: :class:`bool`
-        Whether to run the event loop in debug mode.
-    """
-    try:
-        asyncio.get_running_loop()
-    except RuntimeError:
-        pass
-    else:
-        raise RuntimeError('Cannot call run() from a running event loop.')
-
-    if not asyncio.iscoroutine(coro):
-        raise TypeError('Expected coroutine, not {!}'.format(coro))
-
-    loop = asyncio.new_event_loop()
-    try:
-        asyncio.set_event_loop(loop)
-        loop.set_debug(debug)
-
-        return loop.run_until_complete(coro)
-    finally:
+        Parameters
+        -----------
+        coro: ``Coroutine``
+            The coroutine to run.
+        debug: :class:`bool`
+            Whether to run the event loop in debug mode.
+        """
         try:
-            tasks = asyncio.all_tasks(loop)
+            asyncio.get_running_loop()
+        except RuntimeError:
+            pass
+        else:
+            raise RuntimeError('Cannot call run() from a running event loop.')
 
-            for task in tasks:
-                task.cancel()
+        if not asyncio.iscoroutine(coro):
+            raise TypeError('Expected coroutine, not {!r}'.format(coro))
 
-            loop.run_until_complete(
-                asyncio.gather(*tasks, loop=loop, return_exceptions=True)
-            )
+        loop = asyncio.new_event_loop()
+        try:
+            asyncio.set_event_loop(loop)
+            loop.set_debug(debug)
 
-            for task in tasks:
-                if task.cancelled():
-                    continue
-                if task.exception() is not None:
-                    loop.call_exception_handler({
-                        'message': 'unhandled exception during asyncio.run() shutdown',
-                        'exception': task.exception(),
-                        'task': task,
-                    })
-
-            loop.run_until_complete(loop.shutdown_asyncgens())
-
-            try:
-                loop.run_until_complete(loop.shutdown_default_executor())
-            except AttributeError:
-                pass
+            return loop.run_until_complete(coro)
         finally:
-            asyncio.set_event_loop(None)
-            loop.close()
+            try:
+                tasks = asyncio.all_tasks(loop)
+
+                for task in tasks:
+                    task.cancel()
+
+                loop.run_until_complete(
+                    asyncio.gather(*tasks, return_exceptions=True)
+                )
+
+                for task in tasks:
+                    if task.cancelled():
+                        continue
+                    if task.exception() is not None:
+                        loop.call_exception_handler({
+                            'message': 'unhandled exception during asyncio.run() shutdown',
+                            'exception': task.exception(),
+                            'task': task,
+                        })
+
+                loop.run_until_complete(loop.shutdown_asyncgens())
+            finally:
+                asyncio.set_event_loop(None)
+                loop.close()
