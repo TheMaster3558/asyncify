c05c1d0411632cdda56850ca1bbcfbfc1b2242b7
chawkk6404 <chawk_k@outlook.com>
2022-08-10 08:06:42 -0700

    Fix docs

--- asyncify/sleep.py
+++ asyncify/sleep.py
@@ -31,6 +31,8 @@ async def today_sleep_until(hours: Optional[int] = None, minutes: Optional[int] 
     Sleep until a certain time. The difference between this and :func:`sleep_until` is that this only sleeps within the current day.
 
     .. versionadded:: 1.3
+
+
     Parameters
     -----------
     hours: Optional[:class:`int`]
