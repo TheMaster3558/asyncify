e9ac665845680b599049851ab578b2b88adec1ee
chawkk6404 <chawk_k@outlook.com>
2022-08-30 23:25:41 -0700

    asyncify_class improvements

--- asyncify/cls.py
+++ asyncify/cls.py
@@ -1,7 +1,7 @@
 from __future__ import annotations
 
 import inspect
-from typing import TYPE_CHECKING
+from typing import TYPE_CHECKING, Callable, Container, Optional, overload, Union
 
 from .func import asyncify_func
 
@@ -21,12 +21,39 @@ def ignore(func: CallableT) -> CallableT:
     return func
 
 
-def asyncify_class(cls: TypeT) -> TypeT:
+@overload
+def asyncify_class(
+    cls: None, asyncify: Optional[Container[str]] = None, asyncify_ignore: Optional[Container[str]] = None
+) -> Callable[[TypeT], TypeT]:
+    ...
+
+
+@overload
+def asyncify_class(
+    cls: TypeT, asyncify: Optional[Container[str]] = None, asyncify_ignore: Optional[Container[str]] = None
+) -> TypeT:
+    ...
+
+
+def asyncify_class(
+    cls: Optional[TypeT] = None,
+    asyncify: Optional[Container[str]] = None,
+    asyncify_ignore: Optional[Container[str]] = None,
+) -> Union[TypeT, Callable[[TypeT], TypeT]]:
     """|deco|
+
+    Parameters
+    ----------
+    asyncify: Container[:class:`str`]
+        A container (object with `__contains__`) with the methods to asyncify.
+    asyncify_ignore: Container[:class:`str`]
+        A container with the methods to not asyncify.
+
 
     Turn a classes methods into async functions.
     This uses :func:`asyncify.asyncify_func`.
-    This ignores methods marked with :func:`asyncify.ignore` and `dunder` methods.
+    This ignores methods marked with :func:`asyncify.ignore` and `dunder` methods and methods passed into `asyncify_ignore`.
+    If `asyncify` is passed in, only those methods are asyncified.
 
     Example
     ---------
@@ -35,7 +62,7 @@ def asyncify_class(cls: TypeT) -> TypeT:
         import asyncify
         import requests
 
-        @asyncify.asyncify_class
+        @asyncify.asyncify_class(asyncify=('request', 'get', 'post'))  # don't asyncify random small methods
         class RequestsClient:
             def __init__(self):  # ignored by asyncify
                 self.session = requests.Session()
@@ -54,13 +81,28 @@ def asyncify_class(cls: TypeT) -> TypeT:
     Raises
     -------
     TypeError
-        The object passed was not a class.
+        The object passed was not a class or both `asyncify` and `asyncify_ignore` were passed.
     """
+    if asyncify and asyncify_ignore:
+        raise TypeError('Only asyncify or asyncify_ignore can be passed, not both.')
+
+    if cls is None:
+
+        def inner(inner_cls: TypeT) -> TypeT:
+            return asyncify_class(inner_cls, asyncify=asyncify, asyncify_ignore=asyncify_ignore)
+
+        return inner
+
     if not TYPE_CHECKING and not inspect.isclass(cls):
         raise TypeError(f'Expected class, got {type(cls).__name__!r}')
 
     for name, func in inspect.getmembers(cls):
         if not inspect.isfunction(func) or getattr(func, '_asyncify_ignore', False) or name.startswith('__'):
+            continue
+
+        if asyncify is not None and name not in asyncify:
+            continue
+        if asyncify_ignore is not None and name in asyncify_ignore:
             continue
 
         func = asyncify_func(func)
