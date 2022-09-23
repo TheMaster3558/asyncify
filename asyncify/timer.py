2ad4840dbbfacea1ee041687fd9126506d9b7fa5
chawkk6404 <92063273+chawkk6404@users.noreply.github.com>
2022-09-23 11:35:21 -0700

    Update timer.py

--- asyncify/timer.py
+++ asyncify/timer.py
@@ -1,29 +1,32 @@
 from __future__ import annotations
 
 import asyncio
-from typing import TYPE_CHECKING, Optional
+from typing import TYPE_CHECKING, Any
 
 if TYPE_CHECKING:
     from types import TracebackType
     from typing_extensions import Self
+    
+    
+MISSING: Any = object()   
 
 
 class Timer:
     def __init__(self):
-        self._start_time: Optional[int] = None  #  filled in on __aenter__
-        self._end_time: Optional[int] = None  #  filled in on __aexit__
-        self._loop: asyncio.AbstractEventLoop = asyncio.get_running_loop()
+        self._start_time: int = MISSING
+        self._end_time: int = MISSING
+        self._loop: asyncio.AbstractEventLoop = MISSING
             
     def start_timer(self) -> None:
+        self._loop = asyncio.get_running_loop()
         self._start_time = self._loop.time()
         
     def end_timer(self) -> None:
         self._end_time = self._loop.time()
         
     def get_time(self) -> float:
-        if self._start_time is None or self._end_time is None:
+        if self._start_time is MISSING or self._end_time is MISSING:
             raise RuntimeError('Timer has not ended yet.')
-        
         return self._end_time - self._start_time
     
     async def __aenter__(self) -> Self:
