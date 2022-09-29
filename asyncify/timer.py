4440f98bef7e043e1820ccc8e9a61d7ed05ac16c
chawkk6404 <chawk_k@outlook.com>
2022-09-29 16:36:30 -0700

    Format with black

--- asyncify/timer.py
+++ asyncify/timer.py
@@ -6,9 +6,9 @@ from typing import TYPE_CHECKING, Any, Optional, Type
 if TYPE_CHECKING:
     from types import TracebackType
     from typing_extensions import Self
-    
-    
-MISSING: Any = object()   
+
+
+MISSING: Any = object()
 
 
 class Timer:
@@ -16,23 +16,23 @@ class Timer:
         self._start_time: float = MISSING
         self._end_time: float = MISSING
         self._loop: asyncio.AbstractEventLoop = MISSING
-            
+
     def start_timer(self) -> None:
         self._loop = asyncio.get_running_loop()
         self._start_time = self._loop.time()
-        
+
     def end_timer(self) -> None:
         self._end_time = self._loop.time()
-        
+
     def get_time(self) -> float:
         if self._start_time is MISSING or self._end_time is MISSING:
             raise RuntimeError('Timer has not ended yet.')
         return self._end_time - self._start_time
-    
+
     async def __aenter__(self) -> Self:
         self.start_timer()
         return self
-    
+
     async def __aexit__(
         self,
         exc_type: Optional[Type[BaseException]],
